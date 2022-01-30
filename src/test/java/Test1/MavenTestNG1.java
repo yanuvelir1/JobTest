236c638c59a10d66b3c44813fdaf2f28c373d6eb
Yan Gryshchyshyn <yangri@Yans-MacBook-Air.local>
2022-01-30 18:30:07 +0200

    Initial commit

---
+++ src/test/java/Test1/MavenTestNG1.java
@@ -0,0 +1,26 @@
+package Test1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MavenTestNG1 {
+
+   @Test
+    public void MavenTestNG1 () {
+
+        System.setProperty("webdriver.chrome.driver", "chromedriver");
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://google.com");
+        System.out.println(driver.getTitle());
+
+        String title = driver.getTitle();
+
+        Assert.assertEquals(title, "Google");
+
+        driver.quit();
+
+    }
+}
